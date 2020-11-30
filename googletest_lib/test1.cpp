5cf43b5da16bf2771e0b82f52f3bbaceb50b3086
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2020-11-30 23:48:06 +1100

    more htensor progress

--- googletest_lib/test1.cpp
+++ googletest_lib/test1.cpp
@@ -1,9 +1,9 @@
 #include "gtest/gtest.h"
-// #include "hmath"
+#include "hmath"
 
-// TEST(dft_tests, dft) {
-//     qvec in(16);
-//     in.print_vec();
-//     cvec out = dft(in);
-//     out.print_vec();
-// }
+TEST(dft_tests, dft) {
+    qvec in(16);
+    in.print_vec();
+    cvec out = dft(in);
+    out.print_vec();
+}
